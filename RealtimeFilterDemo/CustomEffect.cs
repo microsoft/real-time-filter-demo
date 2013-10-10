05481f8443027b87ccc77b1ffa4680f1394b6993
Sakari Hyoty <sakari.hyoty@futurice.com>
2013-10-10 14:17:09 +0300

    Add missing copyright header to CustomEffect.cs

--- RealtimeFilterDemo/CustomEffect.cs
+++ RealtimeFilterDemo/CustomEffect.cs
@@ -1,4 +1,12 @@
-﻿using Nokia.Graphics.Imaging;
+﻿/*
+ * Copyright © 2013 Nokia Corporation. All rights reserved.
+ * Nokia and Nokia Connecting People are registered trademarks of Nokia Corporation. 
+ * Other product and company names mentioned herein may be trademarks
+ * or trade names of their respective owners. 
+ * See LICENSE.TXT for license information.
+ */
+
+using Nokia.Graphics.Imaging;
 
 namespace RealtimeFilterDemo
 {
